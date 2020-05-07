0de0372f017dfb3c775ecd978c2a418086a065b9
ARA TA <54147740+Delgadito-bamboo@users.noreply.github.com>
2020-05-07 12:32:43 +0900

    宣言順序の見直し
    mozyさんのアドバイスによる

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
+const status = document.querySelector('#status');
+const duration = document.querySelector('#duration');
 const startButton = document.querySelector('#start');
 const stopButton = document.querySelector('#stop');
-const duration = document.querySelector('#duration');
-const status = document.querySelector('#status');
 const finishMusic = document.querySelector('#finish');
 
 const timer = new Timer(status,duration,startButton,stopButton,finishMusic);
